de0d7c09b03066650f9acd1c5f23e7632f3cf2c8
Adrian <mogekoff@protonmail.ch>
2020-03-07 20:07:42 +0500

    Version 0.4 Unstable Uploaded
    New features:
    - Scripting
    - Arguments of functions
    - Direct executing of functions from main()
    - Some bug fixes
    - Some new bugs

--- iNTER.h
+++ iNTER.h
@@ -7,29 +7,35 @@
 #include <thread>
 #include <chrono>
 #include <csignal>
-
+#include <vector>
 #include <fstream>
 
 #include <queue>
 #include <string>
 #include <map>
+#include <sstream>
+#include <iterator> 
 
 using namespace std;
 
 class iNTER
 {
 private:
-	queue	<function<void()>>						Queue;		//Queue Container of functions
+	queue	<pair<function<void()>,vector<string>>>		Queue;		//Queue Container of functions
 	map		<string, pair<string, function<void()>>>	Functions;	//List of functions: CMDNAME | DESCRIPTION | POINTER
 	static const short EXITCODE = 0;
 	static const short INITCODE = 1;
 	static const short RUNTIME = 2;
+	const string VERSION = "0.4.0 unstable";
+	
+	string CurrentDirectory;
+	
+	vector<string> CurrentParameters;
 
 	int Status;
 	static void SigintHandler(int Signal);
 	static void SigabrtHandler(int Signal);
 
-	string Arguments;
 	static string Time();
 
 	void Executor();											//Executes every command in Queue Container
@@ -37,10 +43,15 @@ private:
 
 public:
 
-	iNTER();													//Contains basic commands
-	int Interpreter();											//Interpreter from stdin and adding to Queue Container
+	iNTER(const int argc, char* argv[]);													//Contains basic commands
+	int Interpreter(istream& STREAM = cin, string Input = "");											//Interpreter from stdin and adding to Queue Container
 
+	int GetArgInt(unsigned NumberOfArgument = 0);
+	string GetArgStr(unsigned NumberOfArgument = 0);
 	static void Print(string Str);								//Formatted print to stdout
+	void ExecuteCommand(string Cmd, vector<string> Parameters = vector<string>());
+	void ExecuteScript(string Dir);
+	
 	void AddFunction(string CmdName,
 		string CmdDescription,
 		function<void()> CmdFunction);
--- main.cpp
+++ main.cpp
@@ -8,14 +8,18 @@ int Foo(int a, int b) {				//Your function
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	iNTER I;
+	iNTER I(argc,argv);
 
 	int Result;
-	I.AddFunction("foo", "Example function", [&Result]() { this_thread::sleep_for(10s); Result = Foo(4, 10);  });
+	I.AddFunction("foo", "Example function", [&Result, &I]() { Result = Foo(I.GetArgInt(), I.GetArgInt(1));  });
 
-	//Try: foo foo exit
+	//Try: "foo 3 5"
+
+	//Autoexec Script before interactive interpreter
+	//Comment this to prevent this execute
+	I.ExecuteScript("ExampleScript.irp");
 
 	return I.Interpreter();
 }
